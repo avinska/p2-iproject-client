00ff731026ee658e0296c479ebf5aa48403ba76d
bbins <avinskat@gmail.com>
2022-11-14 04:53:09 +0700

    refactor: category bar

--- youchub/src/components/CategoryBar.vue
+++ youchub/src/components/CategoryBar.vue
@@ -1,13 +1,57 @@
+<script>
+import { mapActions } from "pinia";
+import { useAllStore } from "../stores/all";
+
+export default {
+  name: "CategoryBar",
+  data() {
+    return {
+      categories: [
+        "All",
+        "Live",
+        "Sewing",
+        "Mixes",
+        "Baking",
+        "Yoga",
+        "Work out",
+        "React routers",
+        "Marine biology",
+        "Cooking shows",
+        "Gaming",
+        "Culinary arts",
+        "Christmas music",
+        "Piano",
+        "Anime",
+      ],
+    };
+  },
+  methods: {
+    ...mapActions(useAllStore, ["fetchVideos"]),
+    handleClick(category) {
+      this.fetchVideos(category);
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="d-flex container-fluid justify-content-center py-3">
-    <button class="btn btn-light mx-1 rounded-pill">All</button>
-    <button class="btn btn-light mx-1 rounded-pill">Live</button>
-    <button class="btn btn-light mx-1 rounded-pill">Sewing</button>
-    <button class="btn btn-light mx-1 rounded-pill">Mixes</button>
-    <button class="btn btn-light mx-1 rounded-pill">Cream</button>
-    <button class="btn btn-light mx-1 rounded-pill">Dough</button>
-    <button class="btn btn-light mx-1 rounded-pill">Baking</button>
-    <button class="btn btn-light mx-1 rounded-pill">React routers</button>
-    <button class="btn btn-light mx-1 rounded-pill">Marine Biology</button>
+  <div class="d-flex container-fluid justify-content-center py-2 w-100 no-wrap">
+    <button
+      v-for="(cat, i) in categories"
+      :key="'cat' + i"
+      class="btn btn-light mx-1 text-decoration-none size-12"
+      @click="handleClick(cat)"
+    >
+      {{ cat }}
+    </button>
   </div>
 </template>
+
+<style scoped>
+.size-12 {
+  font-size: 14px;
+}
+
+.rounder {
+}
+</style>
